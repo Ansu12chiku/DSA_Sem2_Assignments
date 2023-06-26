d4ffd68ee7573f129dc63997d2bf82fad865f8ba
Ansuman Swain <chikuswain2004@gmail.com>
2023-06-26 23:12:38 +0530

    QueueCommit

---
+++ Assignment_7/QueueDemo1withoutGenerics.java
@@ -0,0 +1,91 @@
+package Assignment_7;
+import java.util.*;
+public class QueueDemo1withoutGenerics
+{
+    public static final int MAX = 5;
+    public static int front = -1;
+    public static int rear = -1;
+
+    public static void insert(int Q[]) 
+    {
+        if (is_full()) 
+        {
+            System.out.println("Queue is full, Cannot insert element.");
+            return;
+        }
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the element to insert:");
+        int element =obj.nextInt();
+        if (is_empty())
+            front = 0;
+        Q[++rear] = element;
+        System.out.println("Element " + element + " inserted successfully.");
+    }
+    public static void delete(int Q[]) {
+        if (is_empty()) 
+        {
+            System.out.println("Queue is empty, Cannot delete element...");
+            return;
+        }
+         System.out.println("Element " + Q[front] + " deleted successfully.");
+        if (rear == 0) 
+            front =rear=-1;
+        else 
+        {
+            for (int i = 1; i <=rear; i++)
+                Q[i-1] = Q[i];
+            rear--;
+        }       
+    }
+    public static void display(int Q[]) 
+    {
+        if (is_empty()) 
+        {
+            System.out.println("Queue is empty...Nothing to display");
+            return;
+        }
+        System.out.println("Elements in the queue:");
+        for (int i = front; i <= rear; i++) 
+            System.out.print(Q[i] + " <- ");
+        System.out.println();
+    }
+    public static boolean is_full() 
+    {
+        return rear == MAX - 1;
+    }
+    public static boolean is_empty() 
+    {
+        return front == -1;
+    }
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int queue[]=new int[MAX];
+        while (true) 
+        {
+            System.out.println("***MENU***");
+            System.out.println("0: Exit");
+            System.out.println("1: Insert");
+            System.out.println("2: Delete");
+            System.out.println("3: Display");
+            System.out.println("Enter your choice:");
+            int choice = obj.nextInt();
+            switch (choice) 
+            {
+                case 0:
+                    System.exit(0);
+                case 1:
+                    insert(queue);
+                    break;
+                case 2:
+                    delete(queue);
+                    break;
+                case 3:
+                    display(queue);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
+
---
+++ Assignment_7/QueueDemo2withoutGenerics.java
@@ -0,0 +1,112 @@
+package Assignment_7;
+import java.util.*;
+class Node
+{
+	int info;
+	Node next;
+	Node(int info)
+	{
+		this.info=info;
+		this.next=null;
+	}
+
+}
+public class QueueDemo2withoutGenerics  
+{
+	public static Node insert(Node rear, Node front)
+	{
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter the value to insert in Queue : ");
+        int element=obj.nextInt();
+		Node n = new Node(element);
+		if(rear==null || front==null)
+        {
+			front =n;
+            rear= n;
+        }
+		else
+		{
+			rear.next=n;
+			rear=n;
+		}
+		return front;        
+	}
+	public static  Node delete(Node rear, Node front)
+	{
+		if(front==null)
+			System.out.println("Empty Queue,Nothing to Delete...");
+		else
+		{
+            System.out.println(front.info+" is deleted...");
+			front=front.next;
+		}
+		return front;
+	}
+	public static void display(Node rear, Node front)
+	{
+		Node n = front;
+		if(n==null)
+			System.out.println("Queue is Empty...");
+		else
+		{
+			System.out.println("Elements of Queue are : ");
+			while(n!=null)
+			{
+				System.out.print(n.info+" <- ");
+				n=n.next;
+			}
+            System.out.println();
+		}
+	}
+	public static Node getRear(Node front)
+	{
+		if(front==null)
+		{
+			return null;
+		}
+		else
+		{
+			Node n = front;
+			while(n.next!=null)
+			{
+				n=n.next;
+            }
+			return n;
+		}
+	}
+    public static void main(String args[])
+	{
+		Scanner obj = new Scanner(System.in);
+		Node front=null, rear=null;
+		
+		while(true)
+		{
+			System.out.println("****MENU****");
+			System.out.println("0 : Exit");
+			System.out.println("1 : Insert");
+			System.out.println("2 : Delete");
+			System.out.println("3 : Display");
+			System.out.println("Enter your Choice: ");
+			int choice=obj.nextInt();
+			switch(choice)
+			{
+				case 0:
+					System.exit(0);
+                    System.out.println("Thank you...");
+					break;
+				case 1:
+					front=insert(rear, front);
+					rear=getRear(front);
+					break;
+				case 2:
+					front=delete(rear, front);
+					break;
+				case 3:
+					display(rear, front);
+					break;
+				default:
+					System.out.println("Wrong Choice");
+			}
+		}
+	}
+}
